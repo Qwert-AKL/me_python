72cb33d536f9471da6e43751184441f76f38d0b3
Qwert-AKL <a.l.kambulov@mail.ru>
2024-12-02 23:24:44 +0300

    1

---
+++ PythonProject_modul_2/Call _Counter.py
@@ -0,0 +1,10 @@
+calls = 0
+def count_calls():
+    calls=[]
+    print(calls)
+count_calls()
+
+
+def string_info():
+    string=[]
+def is_contains():
---
+++ PythonProject_modul_2/for.py
@@ -0,0 +1,53 @@
+# while True:
+#     a = int(input('Введите первое число: '))
+#     b = int(input('Введите второе число: '))
+#     summa = a + b
+#     print('Сумма чисел равна: ', summa)
+#     str = str(input('Прервать? нажми клавишу Y  '))
+#     if str == 'Y' or str == 'y' or str == 'Н' or str == 'н':
+#         break
+#     continue
+
+# *******
+#  *   *
+#   * *
+#    *
+#   * *
+#  *   *
+# *******
+# n = 7
+# for i in range(0, n):
+#     for j in range(0, n):
+#         if (i == 0 or i==j or i==n-1):  #i == 0 or i == n - 1 or j == i or j == n - i - 1
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+#  **   **
+# **** ****
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+# h = 7
+# w = h + 2
+# m = w //4
+# for i in range(1, h+1):
+#     if (i <= m):
+#         print(" " * (m-i) + "*" * (2*i) + " " * (w-2*i-2*m) + "*" *(2*i) + " " * (m-i))
+#     else:
+#          print(" " * (i - 2*m+1) + "*" * (w-2*(i-2*m+1)) + " " * (i - 2*m+1))
+
+n=20
+k=1
+counter=1
+for i in range(n):
+    for j in range(n):
+        if k % 2 == 0:
+            print(counter, end ="  ")
+            counter+=1
+        else: print("*",end="  ")
+        k+=1
+    print()

05bdea35db210cea8555c9602281287514b09a81
Qwert-AKL <a.l.kambulov@mail.ru>
2024-12-21 01:49:54 +0300

    *args/ **kwargs

---
+++ PythonProject_modul_2/Однокоренные.py
@@ -0,0 +1,18 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for letter in other_words:
+        if root_word.lower() in letter.lower() or root_word.upper() in letter.upper():
+            same_words.append(letter)
+            continue
+        elif letter.lower() in root_word.lower():
+            same_words.append(letter)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+
+print(result2)

eba96de2b4780573e0b021a5b89e882217897be2
Qwert-AKL <a.l.kambulov@mail.ru>
2025-01-23 16:27:01 +0300

    modul_5_4 (Различие атрибутов класса и экземпляра)

--- PythonProject_modul_5/classandobject.py
+++ PythonProject_modul_5/classandobject.py
@@ -1,4 +1,6 @@
 class Human:
+    head=True
+
     def __init__(self, name, age):  # метод __init__  - инициализация класса
         self.name = name  # определение характеристику.
         self.age = age
@@ -9,25 +11,50 @@ class Human:
 
     def birthday(self):
         self.age += 1
-        print(f'У меня день рождения {self.age} годика')
+        print(f'У меня день рождения,мне теперь {self.age} годика')
 
-
-# print(type(den))
-# print(den.name,den.age,den.surname)
-# print(max.name,max.age)
-# den.say_info()
-# max.say_info()
+    # print(type(den))
+    # print(den.name,den.age,den.surname)
+    # print(max.name,max.age)
+    # den.say_info()
+    # max.say_info()
 
     def __len__(self):
         return self.age
 
+    def __lt__(self, other):  # операторы сравнение методом <
+        return self.age < other.age
+
+    def __gt__(self, other):  # операторы сравнение методом >
+        return self.age > other.age
+
+    def __eq__(self, other):  # операторы сравнение методом ==
+        return self.name == other.name or self.age == other.age
+
+    def __bool__(self):
+        return bool(self.age)
+
+    def __str__(self):
+        return f'{self.name}'
+
     def __del__(self):
         print(f'{self.name} ушёл')
 
-den = Human('Денис', 23)  # den- объект класс Human или Экземпляр класса
-max = Human('Макс', 3)
-# den.surname = "Чубака"
-# del den
-max.birthday()
-print(len(den))
-# del den
+
+den = Human('Денис', 22)  # den- объект класс Human или Экземпляр класса
+max = Human('Макс', 22)
+# # den.surname = "Чубака"
+# # del den
+# # print(den < max)  # вывод сравнения зависит от следующего вызова функции
+# # print(den > max)
+# max.birthday()
+# # print(den < max)  # вывод сравнения перезагрузка оператора сравнения <
+# # print(den > max)  # вывод сравнения перезагрузка оператора сравнения >
+# # print(den == max)  # вывод сравнения перезагрузка оператора сравнения ==
+# # print('<bool>',den == max)  # вывод сравнения перезагрузка оператора сравнения bool
+#
+# # print(len(den))
+# # del den
+# print('вывод методом __str__', den)
+a=6
+print(Human.head)
